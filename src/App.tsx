027f6f3844a74cb0a4de2361890040d64a45693c
maldikurniawan <akurniawan2102@gmail.com>
2026-01-20 12:01:51 +0700

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import About from "./components/pages/about"
+import About from "./components/pages/About"
 import Contact from "./components/pages/Contact"
-import Edu from "./components/pages/edu"
-import Exp from "./components/pages/exp"
+import Edu from "./components/pages/Edu"
+import Exp from "./components/pages/Exp"
 import Project from "./components/pages/Project"
 import Skill from "./components/pages/Skill"
 
 const App = () => {
   return (
-    <div className="relative z-10 max-w-2xl mx-auto py-12 pb-24 sm:py-24 px-6">
+    <div className="relative z-10 max-w-2xl mx-auto py-12 pb-24 sm:py-24 px-6 border">
       <About />
       <Exp />
       <Edu />
